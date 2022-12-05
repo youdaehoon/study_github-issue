a99ebc79665a1250ff453f676afd4c74e41adda4
Dae Hoon YOU <sjssmsqkqh1@gmail.com>
2022-12-05 14:08:07 +0900

    feat: mainpage body create: main.css, common.css
    - 메인 css파일 작성하였습니다. convention에 관해 토의해봅시다.
    - conflict가 일어날 것으로 생각됩니다.
    
    -related:#4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Main from "./pages/main";
 
 function App() {
   return (
     <div className="App">
-
-      
+      <Main />
     </div>
   );
 }
